e19af6639f20a54bdde85b9927c2caf44a6447ae
AzureAD\YevgeniyKovaliov <yevgeniy@shieldfc.com>
2022-03-27 22:37:20 +0300

    change host to localhost

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import abort, Resource, Api
 
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -22,4 +23,4 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(HelloWorldWithName, '/<name>')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='127.0.0.1')
